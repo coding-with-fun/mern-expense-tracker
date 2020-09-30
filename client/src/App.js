fd5d24a82ce271311c01079b0bec2a5bd58cae06
Harsh Patel <harshpatel.softvan@gmail.com>
2020-09-30 18:19:36 +0530

    Optimized the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
-import React from "react";
-import "./App.css";
-import { AddTransaction } from "./components/AddTransaction";
-import { Balance } from "./components/Balance";
-import { Header } from "./components/Header";
-import { IncomeExpenses } from "./components/IncomeExpenses";
-import { TransactionList } from "./components/TransactionList";
-import { GlobalProvider } from "./context/GlobalState";
+import React from 'react';
+import './App.css';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
 	return (
 		<GlobalProvider>
 			<Header />
-			<div className="container">
-				<Balance />
+			<div className='container'>
 				<IncomeExpenses />
 				<TransactionList />
 				<AddTransaction />
